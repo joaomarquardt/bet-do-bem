770c0f049ad9844b8b912b069e23d1dde21bd28b
joaomarquardt <marquardt1402@gmail.com>
2026-02-02 18:36:07 -0300

    initial commit

---
+++ src/main/java/com/api/betdobem/BetdobemApplication.java
@@ -0,0 +1,13 @@
+package com.api.betdobem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetdobemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetdobemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/betdobem/BetdobemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.betdobem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetdobemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
