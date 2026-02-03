1677eb5bdee412fa96845a20781efbec22f5b6a2
joaomarquardt <marquardt1402@gmail.com>
2026-02-02 21:53:20 -0300

    feat: create group entity to store which groups an user belongs to

---
+++ src/main/java/com/api/betdobem/domain/Group.java
@@ -0,0 +1,91 @@
+package com.api.betdobem.domain;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+
+@Table
+@Entity(name = "groups")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id", nullable = false)
+    private User creator;
+
+    private Timestamp createdAt = Timestamp.from(Instant.now());
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_members",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> members;
+
+    public Group() {
+    }
+
+    public Group(Long id, String name, String description, User creator, List<User> members, Timestamp createdAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.creator = creator;
+        this.members = members;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
