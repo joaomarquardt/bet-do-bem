5d750ee3d8b7750c280dfa84ac744e584766c606
joaomarquardt <marquardt1402@gmail.com>
2026-02-05 12:24:07 -0300

    feat: add method to check context type by proofId

--- src/main/java/com/api/betdobem/repositories/ProofRepository.java
+++ src/main/java/com/api/betdobem/repositories/ProofRepository.java
@@ -1,7 +1,19 @@
 package com.api.betdobem.repositories;
 
 import com.api.betdobem.domain.Proof;
+import com.api.betdobem.enums.ContextType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProofRepository extends JpaRepository<Proof, Long> {
+    @Query(value =
+        """
+        SELECT 'BET' as type FROM bet_proofs WHERE proof_id = :id
+        UNION ALL
+        SELECT 'CHALLENGE' as type FROM challenge_proofs WHERE proof_id = :id
+        UNION ALL
+        SELECT 'ACTIVITY' as type FROM activity_proofs WHERE proof_id = :id
+        """, nativeQuery = true)
+    ContextType findContextTypeByProofId(@Param("id") Long proofId);
 }
