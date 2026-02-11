fa68056d27be1195e6e456e3074df2e527daf3d7
joaomarquardt <marquardt1402@gmail.com>
2026-02-11 16:13:26 -0300

    fix: remove unused variable

--- src/main/java/com/api/betdobem/dtos/requests/CreateVoteRequest.java
+++ src/main/java/com/api/betdobem/dtos/requests/CreateVoteRequest.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.NotNull;
 public record CreateVoteRequest(
         @NotNull(message = "Voter ID cannot be null")
         Long voterId,
-        @NotNull(message = "Proof ID cannot be null")
-        Long proofId,
         @NotNull(message = "Approved status cannot be null")
         Boolean approved
 ) {
