a7652958d31c674cfca6d7f0fd33c80cc118f942
joaomarquardt <marquardt1402@gmail.com>
2026-02-11 16:05:54 -0300

    feat: add query method to find a list of entities by status and expires at

--- src/main/java/com/api/betdobem/repositories/ActivityRepository.java
+++ src/main/java/com/api/betdobem/repositories/ActivityRepository.java
@@ -1,14 +1,19 @@
 package com.api.betdobem.repositories;
 
 import com.api.betdobem.domain.Activity;
+import com.api.betdobem.enums.ActivityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
     @Query("SELECT a FROM Activity a WHERE a.proof.id = :proofId")
     Optional<Activity> findByProofId(@Param("proofId") Long proofId);
+
+    List<Activity> findByStatusAndExpiresAtBefore(ActivityStatus status, Timestamp now);
 }
--- src/main/java/com/api/betdobem/repositories/BetRepository.java
+++ src/main/java/com/api/betdobem/repositories/BetRepository.java
@@ -1,10 +1,13 @@
 package com.api.betdobem.repositories;
 
 import com.api.betdobem.domain.Bet;
+import com.api.betdobem.enums.BetStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 public interface BetRepository extends JpaRepository<Bet, Long> {
@@ -15,4 +18,6 @@ public interface BetRepository extends JpaRepository<Bet, Long> {
         WHERE bp.proof_id = :proofId
     """, nativeQuery = true)
     Optional<Bet> findByProofId(@Param("proofId") Long proofId);
+
+    List<Bet> findByStatusAndExpiresAtBefore(BetStatus status, Timestamp now);
 }
