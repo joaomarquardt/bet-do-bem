39cc0d45404feda56da3b650f6fe759fab7f8453
joaomarquardt <marquardt1402@gmail.com>
2026-02-11 10:25:28 -0300

    refactor: change transaction type value to be more semantic

--- src/main/java/com/api/betdobem/enums/TransactionType.java
+++ src/main/java/com/api/betdobem/enums/TransactionType.java
@@ -2,10 +2,10 @@ package com.api.betdobem.enums;
 
 public enum TransactionType {
     DEPOSIT,
-    WITHDRAWAL,
     CHALLENGE_ENTRY,
     CHALLENGE_WIN,
     BET_ENTRY,
     BET_WIN,
+    BET_REFUND,
     REWARD
 }
