3ceb49fd114565c3a145ba9107222ab7e3c1b869
joaomarquardt <marquardt1402@gmail.com>
2026-02-19 18:14:20 -0300

    fix: change selected columns in query

--- src/main/java/com/api/betdobem/repositories/ProofCommentRepository.java
+++ src/main/java/com/api/betdobem/repositories/ProofCommentRepository.java
@@ -1,7 +1,6 @@
 package com.api.betdobem.repositories;
 
 import com.api.betdobem.domain.ProofComment;
-import com.api.betdobem.dtos.responses.CommentResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +9,7 @@ import java.util.List;
 
 public interface ProofCommentRepository extends JpaRepository<ProofComment, Long> {
     @Query("""
-        SELECT c.id, c.content, u.username, u.avatarUrl, c.createdAt
+        SELECT c.id, c.content, u.name, c.postedAt
         FROM ProofComment c
         JOIN c.author u
         WHERE c.proof.id = :proofId
