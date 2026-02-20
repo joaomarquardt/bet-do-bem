ec1b9022cd85ef1e309200bcf16a07f36814f347
joaomarquardt <marquardt1402@gmail.com>
2026-02-20 10:09:51 -0300

    feat: add bean runner to create admin user if not exists yet

---
+++ src/main/java/com/api/betdobem/config/DataInitializer.java
@@ -0,0 +1,31 @@
+package com.api.betdobem.config;
+
+import com.api.betdobem.domain.User;
+import com.api.betdobem.enums.UserRole;
+import com.api.betdobem.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataInitializer {
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
+    @Bean
+    public CommandLineRunner initAdminUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (!userRepository.existsByEmail("admin@admin.com")) {
+                User admin = new User();
+                admin.setName("Admin");
+                admin.setEmail("admin@admin.com");
+                admin.setPassword(passwordEncoder.encode(adminPassword));
+                admin.setRole(UserRole.ADMIN);
+                userRepository.save(admin);
+                System.out.println("User ADMIN created successfully!");
+            }
+        };
+    }
+}
