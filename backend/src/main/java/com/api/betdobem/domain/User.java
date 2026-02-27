0ae62ec456696eafdc3475aeacd74bea6fc50e94
joaomarquardt <marquardt1402@gmail.com>
2026-02-27 12:01:56 -0300

    refactor: save user role enum as string on database

--- backend/src/main/java/com/api/betdobem/domain/User.java
+++ backend/src/main/java/com/api/betdobem/domain/User.java
@@ -18,6 +18,7 @@ public class User implements UserDetails {
     private String name;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private UserRole role;
     private Long coins = 500L;
 
