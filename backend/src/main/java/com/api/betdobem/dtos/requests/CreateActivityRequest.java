d717b38b73a246a618f3ad742b15b700012cd473
joaomarquardt <marquardt1402@gmail.com>
2026-02-27 13:25:35 -0300

    refactor: remove author id in create activity dto

--- backend/src/main/java/com/api/betdobem/dtos/requests/CreateActivityRequest.java
+++ backend/src/main/java/com/api/betdobem/dtos/requests/CreateActivityRequest.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateActivityRequest(
-        @NotNull(message = "Author ID cannot be null")
-        Long authorId, // TODO: Remover authorId daqui
         CreateProofRequest proof,
         @NotBlank(message = "Description cannot be blank")
         String description,
