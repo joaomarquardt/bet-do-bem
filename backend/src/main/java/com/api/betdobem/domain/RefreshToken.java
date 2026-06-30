071c963a01bb8caf6a982ba4b918d2dad6d02157
joaomarquardt <marquardt1402@gmail.com>
2026-06-30 18:01:33 -0300

    fix: map user attribute as many to one intead one to one in refresh token entity

--- backend/src/main/java/com/api/betdobem/domain/RefreshToken.java
+++ backend/src/main/java/com/api/betdobem/domain/RefreshToken.java
@@ -16,7 +16,7 @@ public class RefreshToken {
     @Column(nullable = false)
     private Instant expirationInstant;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
